d40a28f0e38db23917f05c666f01a39c3e2b2fd5
UE2020 <ttmmiizz101@gmail.com>
2023-05-14 21:55:49 -0400

    Finish MCTS

--- src/mcts.rs
+++ src/mcts.rs
@@ -3,9 +3,10 @@ use shakmaty::{
     zobrist::{Zobrist64, ZobristHash},
     *,
 };
-use std::hash::Hash;
 
 pub type BoardHash = u64;
+
+pub const C_PUCT: f32 = 1.2;
 
 fn turn_to_side(color: Color) -> i8 {
     match color {
@@ -17,7 +18,7 @@ fn turn_to_side(color: Color) -> i8 {
 #[derive(Debug, Clone)]
 pub struct Node {
     // Don't point at nodes directly
-    children: Option<Vec<BoardHash>>,
+    children: Option<Vec<(BoardHash, Move)>>,
     visit_count: u32,
     value_sum: f32,
     prior: f32,
@@ -40,7 +41,7 @@ impl Node {
     }
 
     pub fn value(&self) -> f32 {
-        self.value_sum / self.visit_count as f32
+        (self.value_sum / self.visit_count as f32) / 2.0 + 0.5
     }
 
     // pub fn select_child(&self, tree: &MCTSTree) -> (Move, BoardHash) {
@@ -75,16 +76,21 @@ impl MCTSTree {
         self.root = hash.0;
     }
 
-    pub fn expand_node<B: Position, P: FnOnce(&B) -> Vec<(BoardHash, Node)>>(
+    pub fn expand_node<B: Position, P: FnOnce(&B) -> (f32, Vec<(BoardHash, Move, Node)>)>(
         &mut self,
         node: &B,
         prior: P,
-    ) {
-        let new_nodes = prior(node);
+    ) -> f32 {
+        let (value, new_nodes) = prior(node);
         let hash: Zobrist64 = node.zobrist_hash(EnPassantMode::Legal);
         if let Some(node) = self.nodes.get_mut(hash.0) {
-            node.children = Some(new_nodes.iter().map(|(hash, _)| *hash).collect::<Vec<_>>());
-            for (hash, node) in new_nodes {
+            node.children = Some(
+                new_nodes
+                    .iter()
+                    .map(|(hash, m, _)| (*hash, m.clone()))
+                    .collect::<Vec<_>>(),
+            );
+            for (hash, _, node) in new_nodes {
                 self.nodes.insert(hash, node);
             }
         } else {
@@ -93,12 +99,62 @@ impl MCTSTree {
                 node.board().board_fen(Bitboard(0))
             );
         }
+
+        value
     }
 
-    pub fn rollout(&mut self) {
+    pub fn rollout<B: Position + Clone>(&mut self, board: B) -> Result<(), PlayError<B>> {
         let mut search_path = vec![self.root];
-        let mut node = self.root;
-        while self.nodes.get(node).expect("node not found").expanded() {}
+        let mut boards = vec![board.clone()];
+        let mut curr_node = self.root;
+        let mut last_action = None;
+        while self
+            .nodes
+            .get(curr_node)
+            .expect("node not found")
+            .expanded()
+        {
+            let node = self.nodes.get(curr_node).expect("node not found");
+            let child = node.children.as_ref().unwrap().iter().max_by(|a, b| {
+                self.ucb(curr_node, a.0, C_PUCT)
+                    .partial_cmp(&self.ucb(curr_node, b.0, C_PUCT))
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            }).unwrap();
+
+            search_path.push(child.0);
+            curr_node = child.0;
+            last_action = Some(child.1.clone());
+            let last = boards.last();
+            let mut last = last.unwrap().clone();
+            last.play_unchecked(&child.1);
+            boards.push(last);
+        }
+
+        let parent = search_path[search_path.len() - 2];
+        let mut board = boards[boards.len() - 2].clone();
+        board.play_unchecked(&last_action.unwrap());
+        let value = match board.outcome() {
+            Some(Outcome::Decisive { winner }) => turn_to_side(winner) as f32,
+            Some(Outcome::Draw) => 0.0,
+            None => {
+                // expand
+                let value = self.expand_node(boards.last().unwrap(), |_| todo!());
+                if board.turn() == Color::Black {
+                    -value
+                } else {
+                    value
+                }
+            }
+        };
+
+        // back up
+        for node in search_path.iter() {
+            let mut node = self.nodes.get_mut(*node).expect("node not found");
+            node.visit_count += 1;
+            node.value_sum += value * node.to_play as f32;
+        }
+
+        Ok(())
     }
 
     pub fn ucb(&self, parent: BoardHash, child: BoardHash, c_puct: f32) -> f32 {
