0969320eb005b4282853bc15da0d83f9fc19a4d0
Aspect <ttmmiizz100@gmail.com>
2023-05-15 23:42:08 +0000

    Fix Zobrist collisions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use dfdx::prelude::*;
+use std::process::{Command, Stdio};
+use shakmaty::*;
+
 pub mod mcts;
 
 type Mlp = (
@@ -7,9 +10,36 @@ type Mlp = (
     (Linear<32, 2>, Tanh),
 );
 
-fn main() {
-    let dev = AutoDevice::default();
+fn main() -> Result<(), PlayError<Chess>> {
+    let mut child = Command::new("stockfish")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute child");
+
+    let pos = Chess::default();
+    let pos = pos.play(&Move::Normal {
+        role: Role::Pawn,
+        from: shakmaty::Square::D2,
+        to: shakmaty::Square::D4,
+        capture: None,
+        promotion: None,
+    })?;
+    let mut mcts = mcts::MCTSTree::new(&pos, &mut child);
+    for i in 0..100000 {
+        mcts.rollout(pos.clone(), &mut child).unwrap();
+        println!("Rollout {}", i);
+    }
+
+    println!("{}", mcts.pv().iter().map(|m| m.to_uci(pos.castles().mode()).to_string()).collect::<Vec<_>>().join(" "));
+    for (mov, prob) in mcts.root_distribution(&pos) {
+        println!("{}: {:.2}%", mov.to_uci(pos.castles().mode()).to_string(), prob * 100.0);
+    }
+
+    Ok(())
+    
+    /*let dev = AutoDevice::default();
     let mut mlp = dev.build_module::<Mlp, f32>();
 
-    let mut grads = mlp.alloc_grads();
+    let mut grads = mlp.alloc_grads();*/
 }
--- src/mcts.rs
+++ src/mcts.rs
@@ -1,10 +1,12 @@
-use intmap::IntMap;
+use std::collections::HashMap as IntMap;
+use std::hash::Hash;
 use shakmaty::{
-    zobrist::{Zobrist64, ZobristHash},
-    *,
+    zobrist::{Zobrist128, ZobristHash},
+    *, fen::Fen,
 };
-
-pub type BoardHash = u64;
+use vampirc_uci::{parse_one, UciMessage, UciPiece, UciTimeControl};
+use std::process::*;
+use std::io::prelude::*;
 
 pub const C_PUCT: f32 = 1.2;
 
@@ -16,16 +18,16 @@ fn turn_to_side(color: Color) -> i8 {
 }
 
 #[derive(Debug, Clone)]
-pub struct Node {
+pub struct Node<B> {
     // Don't point at nodes directly
-    children: Option<Vec<(BoardHash, Move)>>,
+    children: Option<Vec<(B, Move)>>,
     visit_count: u32,
     value_sum: f32,
     prior: f32,
     to_play: i8,
 }
 
-impl Node {
+impl<B> Node<B> {
     pub fn new(prior: f32, to_play: i8) -> Self {
         Self {
             children: None,
@@ -44,91 +46,164 @@ impl Node {
         (self.value_sum / self.visit_count as f32) / 2.0 + 0.5
     }
 
-    // pub fn select_child(&self, tree: &MCTSTree) -> (Move, BoardHash) {
+    // pub fn select_child(&self, tree: &MCTSTree) -> (Move, B) {
 
     // }
 }
 
 #[derive(Debug, Clone)]
-pub struct MCTSTree {
-    nodes: IntMap<Node>,
-    root: BoardHash,
+pub struct MCTSTree<B: Position + Clone + Eq + PartialEq + Hash> {
+    nodes: IntMap<B, Node<B>>,
+    root: B,
 }
 
-impl MCTSTree {
-    pub fn new<B: Position>(root_board: &B) -> Self {
+impl<B: Position + Clone + Eq + PartialEq + Hash> MCTSTree<B> {
+    pub fn new(root_board: &B, child: &mut Child) -> Self {
         let mut tree = Self {
-            root: 0,
+            root: root_board.clone(),
             nodes: IntMap::new(),
         };
 
         tree.set_root(root_board);
+        tree.expand_node(root_board, |board| {
+            use vampirc_uci::UciInfoAttribute;
+
+            let stdin = child.stdin.as_mut().unwrap();
+            let stdout = child.stdout.as_mut().unwrap();
+            stdin
+                .write_all(
+                    format!("position fen {}\n", Fen::from_position(board.clone(), EnPassantMode::Legal))
+                        .as_bytes(),
+                )
+                .expect("Failed to write to stdin");
+            stdin
+                .write_all("go depth 2\n".as_bytes())
+                .expect("Failed to write to stdin");
+
+            let mut last_value = 0.0;
+            loop {
+                let mut bytes = vec![];
+                loop {
+                    // read a char
+                    let mut output = [0];
+                    stdout
+                        .read_exact(&mut output)
+                        .expect("Failed to read output");
+                    if output[0] as char == '\n' {
+                        break;
+                    }
+                    bytes.push(output[0]);
+                }
+                let output = String::from_utf8_lossy(&bytes);
+                let msg = parse_one(&output);
+                match msg {
+                    UciMessage::Info(attrs) => {
+                        for attr in attrs {
+                            match attr {
+                                UciInfoAttribute::Score { cp, mate, .. } => {
+                                    if let Some(cp) = cp {
+                                        last_value = 2.0
+                                            * (1.0
+                                                / (1.0
+                                                    + 10.0f32
+                                                        .powf(-(cp as f32 / 100.0) / 4.0)))
+                                            - 1.0;
+                                    } else if let Some(mate) = mate {
+                                        if mate > 0 {
+                                            last_value = 1.0 - (mate.abs() as f32 * 0.01);
+                                        } else {
+                                            last_value = -1.0 + (mate.abs() as f32 * 0.01);
+                                        }
+                                        stdin
+                                            .write_all("stop\n".as_bytes())
+                                            .expect("Failed to write to stdin");
+                                        break;
+                                    }
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                    UciMessage::BestMove { .. } => break,
+                    _ => {}
+                }
+            }
+
+            let movs = board.legal_moves();
+
+            (last_value * 2.0 - 1.0, movs.into_iter().map(|m| {
+                let mut board = board.clone();
+                board.play_unchecked(&m);
+                
+                let turn = board.turn();
+
+                (board, m, Node::new(0.0, turn_to_side(turn)))
+            }).collect::<Vec<_>>())
+        });
 
         tree
     }
 
-    pub fn set_root<B: Position>(&mut self, root_board: &B) {
-        let hash: Zobrist64 = root_board.zobrist_hash(EnPassantMode::Legal);
-        if !self.nodes.contains_key(hash.0) {
+    pub fn set_root(&mut self, root_board: &B) {
+        if !self.nodes.contains_key(root_board) {
             self.nodes
-                .insert(hash.0, Node::new(0.0, turn_to_side(root_board.turn())));
+                .insert(root_board.clone(), Node::new(0.0, turn_to_side(root_board.turn())));
         }
-        self.root = hash.0;
+        self.root = root_board.clone();
     }
 
-    pub fn expand_node<B: Position, P: FnOnce(&B) -> (f32, Vec<(BoardHash, Move, Node)>)>(
+    pub fn expand_node<P: FnOnce(&B) -> (f32, Vec<(B, Move, Node<B>)>)>(
         &mut self,
         node: &B,
         prior: P,
     ) -> f32 {
         let (value, new_nodes) = prior(node);
-        let hash: Zobrist64 = node.zobrist_hash(EnPassantMode::Legal);
-        if let Some(node) = self.nodes.get_mut(hash.0) {
+        if let Some(node) = self.nodes.get_mut(node) {
             node.children = Some(
                 new_nodes
                     .iter()
-                    .map(|(hash, m, _)| (*hash, m.clone()))
+                    .map(|(board, m, _)| (board.clone(), m.clone()))
                     .collect::<Vec<_>>(),
             );
-            for (hash, _, node) in new_nodes {
-                self.nodes.insert(hash, node);
+            for (board, _, node) in new_nodes {
+                self.nodes.insert(board, node);
             }
         } else {
             eprintln!(
                 "Warning: node not found - {}",
-                node.board().board_fen(Bitboard(0))
+                Fen::from_position(node.clone(), EnPassantMode::Legal)
             );
         }
 
         value
     }
 
-    pub fn rollout<B: Position + Clone>(&mut self, board: B) -> Result<(), PlayError<B>> {
-        let mut search_path = vec![self.root];
+    pub fn rollout(&mut self, board: B, child: &mut Child) -> Result<(), PlayError<B>> {
+        let mut search_path = vec![self.root.clone()];
         let mut boards = vec![board.clone()];
-        let mut curr_node = self.root;
+        let mut curr_node = self.root.clone();
         let mut last_action = None;
         while self
             .nodes
-            .get(curr_node)
+            .get(&curr_node)
             .expect("node not found")
             .expanded()
         {
-            let node = self.nodes.get(curr_node).expect("node not found");
+            let node = self.nodes.get(&curr_node).expect("node not found");
             let child = node
                 .children
                 .as_ref()
                 .unwrap()
                 .iter()
                 .max_by(|a, b| {
-                    self.ucb(curr_node, a.0, C_PUCT)
-                        .partial_cmp(&self.ucb(curr_node, b.0, C_PUCT))
+                    self.ucb(&curr_node, &a.0, C_PUCT)
+                        .partial_cmp(&self.ucb(&curr_node, &b.0, C_PUCT))
                         .unwrap_or(std::cmp::Ordering::Equal)
                 })
                 .unwrap();
 
-            search_path.push(child.0);
-            curr_node = child.0;
+            search_path.push(child.0.clone());
+            curr_node = child.0.clone();
             last_action = Some(child.1.clone());
             let last = boards.last();
             let mut last = last.unwrap().clone();
@@ -136,7 +211,6 @@ impl MCTSTree {
             boards.push(last);
         }
 
-        let parent = search_path[search_path.len() - 2];
         let mut board = boards[boards.len() - 2].clone();
         board.play_unchecked(&last_action.unwrap());
         let value = match board.outcome() {
@@ -144,7 +218,81 @@ impl MCTSTree {
             Some(Outcome::Draw) => 0.0,
             None => {
                 // expand
-                let value = self.expand_node(boards.last().unwrap(), |_| todo!());
+                let value = self.expand_node(boards.last().unwrap(), |board| {
+                    use vampirc_uci::UciInfoAttribute;
+
+                    let stdin = child.stdin.as_mut().unwrap();
+                    let stdout = child.stdout.as_mut().unwrap();
+                    stdin
+                        .write_all(
+                            format!("position fen {}\n", Fen::from_position(board.clone(), EnPassantMode::Legal))
+                                .as_bytes(),
+                        )
+                        .expect("Failed to write to stdin");
+                    stdin
+                        .write_all("go depth 2\n".as_bytes())
+                        .expect("Failed to write to stdin");
+
+                    let mut last_value = 0.0;
+                    loop {
+                        let mut bytes = vec![];
+                        loop {
+                            // read a char
+                            let mut output = [0];
+                            stdout
+                                .read_exact(&mut output).expect("read failed");
+                            if output[0] as char == '\n' {
+                                break;
+                            }
+                            bytes.push(output[0]);
+                        }
+                        let output = String::from_utf8_lossy(&bytes);
+                        let msg = parse_one(&output);
+                        match msg {
+                            UciMessage::Info(attrs) => {
+                                for attr in attrs {
+                                    match attr {
+                                        UciInfoAttribute::Score { cp, mate, .. } => {
+                                            if let Some(cp) = cp {
+                                                last_value = 2.0
+                                                    * (1.0
+                                                        / (1.0
+                                                            + 10.0f32
+                                                                .powf(-(cp as f32 / 100.0) / 4.0)))
+                                                    - 1.0;
+                                            } else if let Some(mate) = mate {
+                                                if mate > 0 {
+                                                    last_value = 1.0 - (mate.abs() as f32 * 0.01);
+                                                } else {
+                                                    last_value = -1.0 + (mate.abs() as f32 * 0.01);
+                                                }
+                                                stdin
+                                                    .write_all("stop\n".as_bytes())
+                                                    .expect("Failed to write to stdin");
+                                                break;
+                                            }
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                            }
+                            UciMessage::BestMove { .. } => break,
+                            _ => {}
+                        }
+                    }
+
+                    let movs = board.legal_moves();
+
+                    (last_value * 2.0 - 1.0, movs.into_iter().map(|m| {
+                        let mut board = board.clone();
+                        board.play_unchecked(&m);
+                        
+                        let turn = board.turn();
+        
+                        (board, m, Node::new(0.0, turn_to_side(turn)))
+                    }).collect::<Vec<_>>())
+                });
+
                 if board.turn() == Color::Black {
                     -value
                 } else {
@@ -155,7 +303,7 @@ impl MCTSTree {
 
         // back up
         for node in search_path.iter() {
-            let mut node = self.nodes.get_mut(*node).expect("node not found");
+            let mut node = self.nodes.get_mut(node).expect("node not found");
             node.visit_count += 1;
             node.value_sum += value * node.to_play as f32;
         }
@@ -163,9 +311,9 @@ impl MCTSTree {
         Ok(())
     }
 
-    pub fn ucb(&self, parent: BoardHash, child: BoardHash, c_puct: f32) -> f32 {
-        let parent = self.nodes.get(parent).expect("node not found");
-        let child = self.nodes.get(child).expect("node not found");
+    pub fn ucb(&self, parent: &B, child: &B, c_puct: f32) -> f32 {
+        let parent = self.nodes.get(&parent).expect("node not found");
+        let child = self.nodes.get(&child).expect("node not found");
 
         let prior_score = child.prior * c_puct * (parent.visit_count as f32).sqrt()
             / (child.visit_count as f32 + 1.0);
@@ -176,5 +324,51 @@ impl MCTSTree {
         };
 
         value_score + prior_score
+    }
+
+    pub fn pv(&self) -> Vec<Move> {
+        let mut pv = vec![];
+        let mut curr_node = self.root.clone();
+        while self
+            .nodes
+            .get(&curr_node)
+            .expect("node not found")
+            .expanded()
+        {
+            let node = self.nodes.get(&curr_node).expect("node not found");
+            let child = node
+                .children
+                .as_ref()
+                .unwrap()
+                .iter()
+                .max_by(|a, b| {
+                    self.ucb(&curr_node,& a.0, C_PUCT)
+                        .partial_cmp(&self.ucb(&curr_node,& b.0, C_PUCT))
+                        .unwrap_or(std::cmp::Ordering::Equal)
+                })
+                .unwrap();
+            curr_node = child.0.clone();
+            pv.push(child.1.clone());
+        }
+
+        pv
+    }
+
+    pub fn root_distribution(&self, root: &B) -> Vec<(Move, f32)> {
+        let moves = root.legal_moves();
+        let mut distr = vec![];
+        let mut sum = 0;
+        for mov in moves {
+            let mut child = root.clone();
+            child.play_unchecked(&mov);
+            let child_node = self.nodes.get(&child).expect("node not found");
+            distr.push((mov, child_node.visit_count as f32));
+            sum += child_node.visit_count;
+        }
+
+        // rescale
+        distr.iter_mut().for_each(|d| d.1 /= sum as f32);
+
+        distr
     }
 }
