f9623da2b004365b2a20a6dd83aee48e77072fd8
UE2020 <ttmmiizz101@gmail.com>
2023-06-26 14:25:27 -0400

    Prepare new run

--- nn/src/bin.rs
+++ nn/src/bin.rs
@@ -71,11 +71,11 @@ fn main() {
     // return;
 
     let mut grads = model.alloc_grads();
-    let mut opt = Adam::new(
+    let mut opt = Sgd::new(
         &model,
-        AdamConfig {
-            // lr: 0.01,
-            // momentum: Some(Momentum::Nesterov(0.9)),
+        SgdConfig {
+            lr: 0.01,
+            momentum: Some(Momentum::Nesterov(0.9)),
             // weight_decay: Some(WeightDecay::L2(1e-4)),
             ..Default::default()
         },
@@ -84,7 +84,7 @@ fn main() {
     let preprocess =
         |(img, eval, policy, mask): <data::ChessPositionSet as ExactSizeDataset>::Item<'_>| {
             (
-                dev.tensor_from_vec(img, (Const::<16>, Const::<8>, Const::<8>)),
+                dev.tensor_from_vec(img, (Const::<22>, Const::<8>, Const::<8>)),
                 (
                     dev.tensor([eval]),
                     (
@@ -152,7 +152,7 @@ fn main() {
                 let policy = cross_entropy_with_logits_loss(logits.1 * masks.stack(), targets);
                 writer.add_scalar("Value loss", value.array(), total_training_steps as usize);
                 writer.add_scalar("Policy loss", policy.array(), total_training_steps as usize);
-                let loss = value + policy;
+                let loss = (value * 0.01) + policy;
 
                 writer.add_scalar("Training loss", loss.array(), total_training_steps as usize);
 
