a591fb17579b4502c64c7a761dc85b56f22cf8ad
UE2020 <ttmmiizz101@gmail.com>
2023-06-28 18:04:40 -0400

    Fix training path

--- nn/src/bin.rs
+++ nn/src/bin.rs
@@ -101,7 +101,7 @@ fn main() {
 
     for i_epoch in 0..7 {
         let file =
-            File::open("nn/data/lichess_elite_2021-11.pgn").expect("training data not found");
+            File::open("nn/data/lichess_elite_2021-10.pgn").expect("training data not found");
         let mut reader = BufferedReader::new(file);
         let mut visitor = data::PgnVisitor::new();
 
